1ea7589a020715aef7aaa1b002775ab26c6fdacd
Rohit Bhoompally <rohit.bhoompally@gmail.com>
2015-01-03 20:38:59 -0500

    Starter frames added.

---
+++ app/src/main/java/com/rohitbhoompally/tack/FrameImageAdapter.java
@@ -0,0 +1,62 @@
+package com.rohitbhoompally.tack;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+/**
+ * Created by Rohit on 1/3/15.
+ */
+public class FrameImageAdapter extends BaseAdapter {
+    private Context mContext;
+    static int mSelectedPosition;
+
+    public FrameImageAdapter(Context c) {
+        mContext = c;
+        mSelectedPosition = 0;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {  // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(275, 275));
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            imageView.setPadding(5, 5, 5, 5);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        if(position == mSelectedPosition)
+            imageView.setImageResource(mThumbIdsSelected[position]);
+        else
+            imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.frame_2_h, R.drawable.frame_2_v, R.drawable.frame_3_h, R.drawable.frame_3_v, R.drawable.frame_4_vh
+    };
+
+    // references to selected images
+    private Integer[] mThumbIdsSelected = {
+            R.drawable.frame_2_h_selected, R.drawable.frame_2_v_selected, R.drawable.frame_3_v_selected, R.drawable.frame_3_h_selected, R.drawable.frame_4_vh_selected
+    };
+}
--- app/src/main/java/com/rohitbhoompally/tack/LayoutFragment.java
+++ app/src/main/java/com/rohitbhoompally/tack/LayoutFragment.java
@@ -1,15 +1,34 @@
 package com.rohitbhoompally.tack;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 /**
  * Created by Rohit Bhoompally on 12/8/14.
  */
 public class LayoutFragment extends Fragment {
+
+    private Context mContext;
+    private FrameImageAdapter mFrameImageAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getActivity();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mContext = getActivity();
+    }
 
     public static LayoutFragment newInstance() {
         LayoutFragment layoutFragment = new LayoutFragment();
@@ -20,6 +39,17 @@ public class LayoutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_layout, container, false);
+
+        final GridView gridview = (GridView) view.findViewById(R.id.gridview);
+        mFrameImageAdapter = new FrameImageAdapter(mContext);
+        gridview.setAdapter(mFrameImageAdapter);
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                FrameImageAdapter.mSelectedPosition = position;
+                mFrameImageAdapter.notifyDataSetChanged();
+            }
+        });
         return view;
     }
 }
